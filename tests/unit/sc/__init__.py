c099851de23d8529f1b59118a8106b05168bceb0
bobbyc <bobby.chien@gmail.com>
2012-12-14 08:55:23 +0800

    add test suite

--- tests/unit/sc/__init__.py
+++ tests/unit/sc/__init__.py
@@ -1,17 +1,11 @@
-#
-# Copyright (C) 2012 by Trend Micro Inc, All rights are reserved. 
-#
-# Reproduction or transmission in whole or in part, in any form or by 
-# any means, electronic, mechanical or otherwise, is prohibited without 
-# the prior written permission of the copyright owner.
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-# 
-# THIS PLUGIN IS PROVIDED "AS IS". TREND MICRO MAKES NO WARRANTY OR 
-# PROMISE ABOUT THE OPERATION OR PERFORMANCE OF THIS PLUGIN NOR DOES 
-# IT WARRANT THAT THIS PLUGIN IS ERROR FREE. TO THE FULLEST EXTENT 
-# PERMITTED BY LAW, TREND MICRO DISCLAIMS ALL IMPLIED AND STATUTORY 
-# WARRANTIES, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF 
-# MERCHANTABILITY, NONINFRINGEMENT AND FITNESSFOR A PARTICULAR PURPOSE.
-#
+import unittest
+import tests.unit.sc.test_device as DeviceTest
+import tests.unit.sc.test_scconnection as ConnectionTest
+
+
+class   SCTestSuite(unittest.TestSuite):
+    def __init__(self):
+        unittest.TestSuite.__init__(self, map(ConnectionTest.SCConnectionTest(), ('testListAllDevice')))
+    
+if __name__ == '__main__':
+    unittest.main()
