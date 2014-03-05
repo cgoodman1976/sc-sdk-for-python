43dfac8e17c4d5966a9d6a6023966401de9437a3
bobbyc <bobby.chien@gmail.com>
2014-03-05 20:12:32 +0800

    update connection test case

--- tests/unit/test_connection.py
+++ tests/unit/test_connection.py
@@ -1,13 +1,35 @@
 import unittest
-from tests.unit import config
+import sclib
+import os
+import tests
 
 
 class SCAuthConnectionTest(unittest.TestCase):
+    def __init__(self, methodName='runTest'):
+        unittest.TestCase.__init__(self, methodName)
+        self.connection = None
+        self.auth = None
+        
+        # make sample path
+        self.sample_path = os.path.join(os.path.dirname(os.path.abspath(tests.__file__ )), 'unit', 'sample')
+        # make result path
+        self.result_path = os.path.join(os.path.dirname(os.path.abspath(tests.__file__ )), 'unit', 'result')
+
     def setUp(self):
+
+        path = os.path.join(self.result_path, '%s.%s' % (self.__class__.__name__, self._testMethodName) )
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    	# enable debug log
+        path = os.path.join(self.result_path, '%s.%s' % (self.__class__.__name__, self._testMethodName) )
+        logpath = os.path.join(path, 'testCase.log' )
+        sclib.set_file_logger(self._testMethodName, logpath, 'DEBUG')
+
         from sclib.connection import SCAuthConnection
-        self.connection = SCAuthConnection( config.get('connection', 'MS_HOST'),
-                                            config.get('connection', 'MS_BROKER_NAME'), 
-                                            config.get('connection', 'MS_BROKER_PASSPHASE') )
+        self.connection = SCAuthConnection( sclib.__config__.get('connection', 'MS_HOST'),
+                                            sclib.__config__.get('connection', 'MS_BROKER_NAME'), 
+                                            sclib.__config__.get('connection', 'MS_BROKER_PASSPHASE') )
 
     def testGetRequest(self):
         response = self.connection.make_request('PublicCertificate')
